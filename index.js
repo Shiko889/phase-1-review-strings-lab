cbe4be22625b7cd41ea6a8c3cd08dd315b9566c5
shiko <sarah.tharao@student.moringaschool.com>
2023-12-21 15:02:25 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Write your code in this file!
+const currentUser = 'Sarah';
+const welcomeMessage = 'Welcome to Flatbook, ${Sarah}!';
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const shortGreeting = 'Welcome, ${currentUser.Slice(0,1)}!';
+ 
